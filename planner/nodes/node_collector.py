bcd7b9c98fb2674e4fc412d0bf96ad401f426962
zelima <irakli.mchedlishvili@datopian.com>
2017-10-29 14:31:38 +0400

    [bugfix][s]: not consume allowed artifacts before it is yielded

--- planner/nodes/node_collector.py
+++ planner/nodes/node_collector.py
@@ -3,6 +3,8 @@ from .basic_nodes import DerivedCSVProcessingNode, DerivedJSONProcessingNode, \
 from .view_nodes import DerivedPreviewProcessingNode
 from .output_nodes import OutputToZipProcessingNode
 
+import itertools
+
 ORDERED_NODE_CLASSES = [
     DerivedCSVProcessingNode,
     DerivedPreviewProcessingNode,
@@ -18,5 +20,7 @@ def collect_artifacts(artifacts, outputs, allowed_types=None):
         ret = list(node.get_artifacts())
         if allowed_types is not None:
             ret = filter(lambda a: a.datahub_type in allowed_types, ret)
-        artifacts.extend(ret)
-        yield from ret
+        # Need to make copy of iter as it is consumed in artifacts.extend()
+        for_artifacts, for_use = itertools.tee(ret)
+        artifacts.extend(for_artifacts)
+        yield from for_use
